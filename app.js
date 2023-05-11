f44b1d2da3aae3457940e11713da818e25fbfd46
Sunkari Vasu <vasu@universaltennis.com>
2023-05-11 15:43:34 +0530

    changed mongoDB uri 2

--- app.js
+++ app.js
@@ -142,7 +142,7 @@ cron.schedule("40 16 * * *",() =>
 });
 })
 
-mongoose.connect('mongodb+srv://admin-vasu:vasu%40143@cluster0.ypfh3.mongodb.net/CitySuperMarketDB?retryWrites=true&w=majority',{useNewUrlParser:true},(err) =>{
+mongoose.connect(process.env.URI,{useNewUrlParser:true},(err) =>{
     if(err)
         console.log("Error while connecting to database:"+err);
     else
